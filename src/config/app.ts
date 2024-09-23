26302777320004fdf420b14128da58a51f6b25ed
Afranio Caires <137647669+AfranioCaires@users.noreply.github.com>
2024-09-22 22:18:41 -0300

    Refactor: server configuration and add app.ts

---
+++ src/config/app.ts
@@ -0,0 +1,61 @@
+import Fastify from "fastify";
+import "dotenv/config";
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+import eventPlugin from "../plugins/eventBus";
+import { setupEvents } from "../events/setupEvents";
+import { ridesController } from "../controllers/rides/ridesController";
+import { userController } from "../controllers/users/userController";
+import { vehicleController } from "../controllers/vehicles/vehicleController";
+import { reservationController } from "../controllers/reservations/reservationController";
+import jwtPlugin from "../plugins/jwtPlugin";
+import { authController } from "../controllers/auth/auth";
+import { reviewsController } from "../controllers/reviews/reviewController";
+import websocket from "@fastify/websocket";
+import { messageController } from "../controllers/messages/messageController";
+import fastifyMultipart from "@fastify/multipart";
+import { ZodError } from "zod";
+
+const app = Fastify();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(fastifyMultipart);
+app.register(jwtPlugin);
+app.register(userController);
+app.register(vehicleController);
+app.register(ridesController);
+app.register(reservationController);
+app.register(authController);
+app.register(reviewsController);
+app.register(websocket);
+app.register(eventPlugin);
+app.register(messageController);
+
+app.setErrorHandler((error, request, reply) => {
+  if (error instanceof ZodError) {
+    reply.status(400).send({
+      statusCode: 400,
+      error: "Bad Request",
+      issues: error.issues,
+    });
+    return;
+  }
+
+  reply.send(error);
+});
+
+app.ready((err) => {
+  if (err) {
+    console.error("Erro ao iniciar o Fastify:", err);
+    process.exit(1);
+  }
+  setupEvents(app);
+
+  console.log("Servidor pronto para receber requisições.");
+});
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,44 +1,4 @@
-import Fastify from "fastify";
-import "dotenv/config";
-
-import {
-  serializerCompiler,
-  validatorCompiler,
-} from "fastify-type-provider-zod";
-import eventPlugin from "./plugins/eventBus";
-import { setupEvents } from "./events/setupEvents";
-import { ridesController } from "./controllers/rides/ridesController";
-import { userController } from "./controllers/users/userController";
-import { vehicleController } from "./controllers/vehicles/vehicleController";
-import { reservationController } from "./controllers/reservations/reservationController";
-import jwtPlugin from "./plugins/jwtPlugin";
-import { loginController } from "./controllers/authController/auth";
-import { reviewsController } from "./controllers/reviews/reviewController";
-
-const app = Fastify();
-
-app.setValidatorCompiler(validatorCompiler);
-app.setSerializerCompiler(serializerCompiler);
-
-app.register(jwtPlugin);
-app.register(userController);
-app.register(vehicleController);
-app.register(ridesController);
-app.register(reservationController);
-app.register(loginController);
-app.register(reviewsController);
-
-app.register(eventPlugin);
-
-app.ready((err) => {
-  if (err) {
-    console.error("Erro ao iniciar o Fastify:", err);
-    process.exit(1);
-  }
-
-  setupEvents(app);
-  console.log("Servidor pronto para receber requisições.");
-});
+import app from "./config/app";
 
 app
   .listen({ port: 3000 })
@@ -49,5 +9,3 @@ app
     console.error("Falha ao iniciar o servidor:", err);
     process.exit(1);
   });
-
-export default app;
